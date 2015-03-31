66f328b328a80fd26adcb4a44c18bacdef08d089
Vanessa <vmangion@gmail.com>
2015-03-31 20:17:16 +0200

    Software Seller 1

---
+++ SoftwareSeller/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SoftwareSeller
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ SoftwareSeller/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SoftwareSeller.MvcApplication" Language="C#" %>
